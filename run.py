57fa66646a8b2a0118009f73033357c58dc2fcf3
geyingqi <geyingqi88@88.com>
2024-07-25 23:49:07 +0800

    Changes

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from commons.yaml_util import write_yaml, read_yaml, clear_yaml
 
 if __name__ == '__main__':
     pytest.main()
-    time.sleep(3)
+    time.sleep(4)
     os.system("allure generate ./temps -o ./reports --clean")
     #write_yaml("a")
     #read_yaml()
